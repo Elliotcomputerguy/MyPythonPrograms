0390ea941ba7cd751d11105edd506f8e4d7ddf2f
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-17 18:57:25 +0100

    Times table program

--- TimesTable.py
+++ TimesTable.py
@@ -3,8 +3,7 @@
 # My simple times table program
 
 i = 1
-print('Enter your times table of choice:')
-timestable = input()
+timestable = input('Enter your times table of choice:')
 timestable = int(timestable)
 while i <=10:
     print('10 x', i, '=', i * timestable )
