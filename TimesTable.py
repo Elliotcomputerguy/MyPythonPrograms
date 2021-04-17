659315731d37879069e5ba207eb54aaf32389814
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-17 21:25:24 +0100

    Times table program

--- TimesTable.py
+++ TimesTable.py
@@ -6,6 +6,6 @@ i = 1
 timestable = input('Enter your times table of choice:')
 timestable = int(timestable)
 while i <=10:
-    print('10 x', i, '=', i * timestable )
+    print(timestable, 'x', i, '=', i * timestable )
     i = i + 1
     
