e4b2b3f53e5436622c38d239048b6d58484d54d3
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-18 18:04:20 +0100

    Ceaser Cipher Hacker

---
+++ CeaserCipherHacker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Ceaser cipher Hacker
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+message = input('Enter encrypted cipher to hack:')
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(LETTERS)):
+    translated = ''
+
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = num - key
+
+            if num < 0:
+                num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+    print('Key #%s: %s' % (key, translated))
