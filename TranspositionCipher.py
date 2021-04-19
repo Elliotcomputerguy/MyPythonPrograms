6cb957acbce3bf2165935c1a4da24489f7cd08c0
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-19 19:35:01 +0100

    Transposition Cipher

---
+++ TranspositionCipher.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+def main():
+    myMessage = input('Enter message:')
+    myKey = input('Enter encryption key:')
+    myKey = int(myKey)
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    print(ciphertext + '|')
+
+def encryptMessage(key, message):
+
+    ciphertext = [''] * key
+
+    for col in range(key):
+        pointer = col
+
+        while pointer < len(message):
+
+            ciphertext[col] += message[pointer]
+
+            pointer += key
+    
+    return ''.join(ciphertext)
+
+if __name__ == '__main__':
+    main()
