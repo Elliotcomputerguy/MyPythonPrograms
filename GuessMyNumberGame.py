1f5b3399a752c1328388944f39cdd67a07b02422
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-21 23:02:49 +0100

    Guess The Number Game

---
+++ GuessMyNumberGame.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import random, sys, os, time
+
+def main():
+    os.system('cls')
+    print('''
+
+ ██████╗ ██╗   ██╗███████╗███████╗███████╗    ████████╗██╗  ██╗███████╗    ███╗   ██╗██╗   ██╗███╗   ███╗██████╗ ███████╗██████╗      ██████╗  █████╗ ███╗   ███╗███████╗
+██╔════╝ ██║   ██║██╔════╝██╔════╝██╔════╝    ╚══██╔══╝██║  ██║██╔════╝    ████╗  ██║██║   ██║████╗ ████║██╔══██╗██╔════╝██╔══██╗    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
+██║  ███╗██║   ██║█████╗  ███████╗███████╗       ██║   ███████║█████╗      ██╔██╗ ██║██║   ██║██╔████╔██║██████╔╝█████╗  ██████╔╝    ██║  ███╗███████║██╔████╔██║█████╗  
+██║   ██║██║   ██║██╔══╝  ╚════██║╚════██║       ██║   ██╔══██║██╔══╝      ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
+╚██████╔╝╚██████╔╝███████╗███████║███████║       ██║   ██║  ██║███████╗    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██████╔╝███████╗██║  ██║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
+ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+                                                                                                                                                                         
+\t\t\t\tWelcome to the guess the number game. Pick a number between 1 and 100. To exit type exit.
+    ''')
+    randomNumber = random.randint(1, 100)
+    playerGuesses = 1
+    while True:
+        playerGuess = input('Guess a number between 1 and a 100:')
+        playerExit = playerGuess.upper()
+        if playerExit == 'EXIT':
+            print('\nThanks for playing the Guess The Number Game')
+            time.sleep(6)
+            sys.exit()
+        else:
+            playerGuess = int(playerGuess)
+        while playerGuess != randomNumber:
+            if playerGuess > randomNumber:
+                print('Your guess %s is to high' % (playerGuess))
+            elif playerGuess < randomNumber:
+                print('Your guess %s is to low' % (playerGuess))
+            playerGuess = input('Guess a number between 1 and a 100:')
+            playerGuess = int(playerGuess)
+            playerGuesses += 1
+        print('\nYou have guessed the number %s it took you %s guesses ' % (playerGuess, playerGuesses))
+        time.sleep(6)
+main()       
