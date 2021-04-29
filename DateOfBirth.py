8f88e81a022bed8644d1ee9c914fa829501b483f
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-04-29 01:00:50 +0100

    Date Of Birth

--- DateOfBirth.py
+++ DateOfBirth.py
@@ -6,20 +6,28 @@ userDob = input('Please enter your date of Birth in DD-MM-YYYY:')
 for i in userDob:
     if '0' in userDob:
         userDob = userDob.replace('0','a')
-    else:
-        userDay = userDob[0:2] 
-        userMonth = userDob[2:4]
-        userYear = userDob[4:]
-if userDob[0:1] == 'a' and userDob[2:3] == 'a':
+if 'a' not in userDob:
+    userDay = userDob[0:2] 
+    userMonth = userDob[2:4]
+    userYear = userDob[4:]
+
+elif userDob[0:1] == 'a' and userDob[2:3] == 'a':
     userDob = userDob.replace('a', '')
-    userDay = userDob[0:1] 
+    userDay = userDob[0:1]
     userMonth = userDob[2:3]
     userYear = userDob[2:]
-elif 'a' == userDob[0:1] and userDob[3:4]:
-    userDob = userDob.replace('0', '')
+
+elif userDob[0:1] == 'a' and userDob[3:4] =='a':
+    userDob = userDob.replace('a', '')
     userDay = userDob[0:1]
     userMonth = userDob[3:4]
-    userYear = userDob[2:]    
+    userYear = userDob[2:]
+
+elif userDob[0:1] == 'a':
+    userDob = userDob.replace('a', '')
+    userDay = userDob[0:1]
+    userMonth = userDob[2:4]
+    userYear = userDob[3:] 
 
 currentAge  = int(dt.day) - int(userDay) and int(dt.month) - int(userMonth) and int(dt.year) - int(userYear)
 futureYear = int(dt.year) + 10
