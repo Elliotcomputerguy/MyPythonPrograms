74d65d73e4d469d7db40f6565b30cd653d80c9a6
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-04-29 11:16:41 +0100

    Date Of Birth 2

--- DateOfBirth2.py
+++ DateOfBirth2.py
@@ -7,12 +7,12 @@ if '0' in userDob:
     userMonth = userDob[3:6]
     userYear = userDob[6:]
     currentAge  = float(dt.day) - float(userDay) and float(dt.month) - float(userMonth) and int(dt.year) - int(userYear)
-    print(math.floor(currentAge))
 else:
     userDay = userDob[0:2] 
     userMonth = userDob[2:4]
     userYear = userDob[4:]
     currentAge  = int(dt.day) - int(userDay) and int(dt.month) - int(userMonth) and int(dt.year) - int(userYear)
+
 
 futureYear = int(dt.year) + 10
 print('Your age is ' + str(currentAge) + '\nYou will be ' + str(int(currentAge) + int(10)) + ', 10 years from now in the year ' + str(futureYear))
