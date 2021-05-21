ea6e3bdace8c443f34f5373e6490f0d0fe1de23c
elliotcomputerguy <elliot@elliotcomputerguy.com>
2021-05-21 23:58:55 +0100

    Date Of Birth v2

--- DateOfBirth2.py
+++ DateOfBirth2.py
@@ -1,5 +1,9 @@
 import datetime
 
+def currentdate():
+    currentDateYear = datetime.datetime.today().year
+    return currentDateYear
+
 def dateOfBirth(date):
     currentDate = datetime.datetime.today()
     if '0' in date:
@@ -18,25 +22,9 @@ def dateOfBirth(date):
 
 def main():
     dob = input('Please enter your date of Birth in DD-MM-YYYY:')
-    futureYear = int(dt.year) + 10
+    currentAge = dateOfBirth(dob)
+    futureYear = int(currentdate()) + 10
+    #futureYear = int(dt.year) + 10
     print('Your age is ' + str(currentAge) + '\nYou will be ' + str(int(currentAge) + int(10)) + ', 10 years from now in the year ' + str(futureYear))
 
-
-
-dt = datetime.datetime.today()
-userDob = input('Please enter your date of Birth in DD-MM-YYYY:') 
-if '0' in userDob:
-    userDob = userDob.replace('0','.0')
-    userDay = userDob[0:3] 
-    userMonth = userDob[3:6]
-    userYear = userDob[6:]
-    currentAge  = float(dt.day) - float(userDay) and float(dt.month) - float(userMonth) and int(dt.year) - int(userYear)
-else:
-    userDay = userDob[0:2] 
-    userMonth = userDob[2:4]
-    userYear = userDob[4:]
-    currentAge  = int(dt.day) - int(userDay) and int(dt.month) - int(userMonth) and int(dt.year) - int(userYear)
-
-
-futureYear = int(dt.year) + 10
-print('Your age is ' + str(currentAge) + '\nYou will be ' + str(int(currentAge) + int(10)) + ', 10 years from now in the year ' + str(futureYear))
+main()
