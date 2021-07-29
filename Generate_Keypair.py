ec179469ee30954bd13549d5ef0a40257d892d2d
elliot <elliot@elliotcomputerguy.com>
2021-07-29 10:11:31 +0100

    Generate Key Pair

--- Generate_Keypair.py
+++ Generate_Keypair.py
@@ -1,8 +1,21 @@
+
 def gen_key(server):
-    import os
+    import os, getpass
     serverName = server
     cmdpass = f'ssh-keygen -f id_rsa.{serverName}'
     os.system(cmdpass)
+
+    if os.name == 'nt':
+        print('Detected Windows operating system....')
+        print('Make directory \.ssh')
+        os.system('mkdir C:\Users\username\.ssh')
+        username = getpass.getuser()
+        os.system(f' id_rsa.{serverName} C:\Users\{username}\.ssh\id_rsa.{serverName}.pub')
+        os.system(f' id_rsa.{serverName} C:\Users\{username}\.ssh\id_rsa.{serverName}')
+        print('Your public key file and private key file has been copied to c:\users\{username}\.ssh')
+        print('Copy your public key to your server to authenticate against using your private key')
+    elif os.name == '':
+        pass
    # os.system(f'{cmdpass} >> ~/.ssh/authorized_keys')
 
 def main():
